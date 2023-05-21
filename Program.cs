f1c4a026e0367042c84a6b8dab5d7b534e3f3aca
Kassia Fraga <kassia.fraga@lumaensino.com.br>
2023-05-21 20:48:17 -0300

    CRUD

--- Program.cs
+++ Program.cs
@@ -23,5 +23,61 @@ namespace Blog
         }
       }
     }
+
+    public static void ReadUser()
+    {
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var user = connection.Get<User>(1);
+        Console.WriteLine(user.Name);
+      }
+    }
+
+    public static void CreateUser()
+    {
+      var user = new User()
+      {
+        Bio = "Desenvolvedora FullStack",
+        Email = "dev@fullstack.com",
+        Image = "https://...",
+        Name = "Uma dev aí",
+        PasswordHash = "HASH",
+        Slug = "uma-dev-ai"
+      };
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        connection.Insert<User>(user);
+        Console.WriteLine("Cadastro realizado com sucesso");
+      }
+    }
+
+    public static void UpdateUser()
+    {
+      var user = new User()
+      {
+        Id = 2,
+        Bio = "Desenvolvedora FullStack",
+        Email = "dev@fullstack.com",
+        Image = "https://...",
+        Name = "DEV",
+        PasswordHash = "HASH",
+        Slug = "dev"
+      };
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        connection.Update<User>(user);
+        Console.WriteLine("Atualização realizada com sucesso");
+      }
+    }
+
+    public static void DeleteUser()
+    {
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var user = connection.Get<User>(2);
+        connection.Delete<User>(user);
+        Console.WriteLine("Exclusão realizada com sucesso");
+      }
+    }
   }
 }
