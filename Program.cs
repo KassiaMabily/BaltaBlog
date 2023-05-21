bf44c6bb7bf549fb0672906be5aeb0b9deb049fc
Kassia Fraga <kassia.fraga@lumaensino.com.br>
2023-05-21 20:37:09 -0300

    Create user model read

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+  class Program
+  {
+    private const string CONNECTION_STRING = "Server=localhost,1432;Database=Blog;User ID=sa;Password=Serif@123;Trusted_Connection=False;Encrypt=False;MultipleActiveResultSets=true;";
+    static void Main(string[] args)
+    {
+      ReadUsers();
+    }
+
+    public static void ReadUsers()
+    {
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var users = connection.GetAll<User>();
+        foreach (var user in users)
+        {
+          Console.WriteLine(user.Name);
+        }
+      }
+    }
+  }
+}
