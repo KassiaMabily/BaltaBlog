09f1f7da08f1ab0cd91afaddd006eff0a661448c
Kassia Fraga <kassia.fraga@lumaensino.com.br>
2023-05-21 21:57:51 -0300

    Create roleRepo

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace Blog
       var connection = new SqlConnection(CONNECTION_STRING);
       connection.Open();
       ReadUsers(connection);
+      ReadRoles(connection);
       connection.Close();
     }
 
@@ -26,60 +27,16 @@ namespace Blog
 
     }
 
-    public static void ReadUser()
+    public static void ReadRoles(SqlConnection connection)
     {
-      using (var connection = new SqlConnection(CONNECTION_STRING))
-      {
-        var user = connection.Get<User>(1);
-        Console.WriteLine(user.Name);
-      }
+      var repository = new RoleRepository(connection);
+      var roles = repository.Get();
+
+      foreach (var role in roles)
+        Console.WriteLine(role.Name);
+
     }
 
-    public static void CreateUser()
-    {
-      var user = new User()
-      {
-        Bio = "Desenvolvedora FullStack",
-        Email = "dev@fullstack.com",
-        Image = "https://...",
-        Name = "Uma dev aí",
-        PasswordHash = "HASH",
-        Slug = "uma-dev-ai"
-      };
-      using (var connection = new SqlConnection(CONNECTION_STRING))
-      {
-        connection.Insert<User>(user);
-        Console.WriteLine("Cadastro realizado com sucesso");
-      }
-    }
 
-    public static void UpdateUser()
-    {
-      var user = new User()
-      {
-        Id = 2,
-        Bio = "Desenvolvedora FullStack",
-        Email = "dev@fullstack.com",
-        Image = "https://...",
-        Name = "DEV",
-        PasswordHash = "HASH",
-        Slug = "dev"
-      };
-      using (var connection = new SqlConnection(CONNECTION_STRING))
-      {
-        connection.Update<User>(user);
-        Console.WriteLine("Atualização realizada com sucesso");
-      }
-    }
-
-    public static void DeleteUser()
-    {
-      using (var connection = new SqlConnection(CONNECTION_STRING))
-      {
-        var user = connection.Get<User>(2);
-        connection.Delete<User>(user);
-        Console.WriteLine("Exclusão realizada com sucesso");
-      }
-    }
   }
 }
